4ae94bc602b790ad4c90ae606a3b75fc4585f4f1
Haneen AbuKhater <abukhater.haneen@gmail.com>
2017-07-05 11:40:40 -0700

    initial html, js files added

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+//Business Logic
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var number1 = parseInt(prompt("enter a number: "));
+var number2 = parseInt(prompt("enter another number: "));
+
+alert(add(number1, number2));
+
+//this is a comment
+
+/*var age = prompt ("enter your age: ");
+var name = prompt("enter your name: ");
+var faveFood = prompt("enter your favorite food");
+
+var greeting = function(whattosay) {
+  alert(whattosay);
+}
+greeting("Hi " + name + "! Your are " + age + " years old! And your favorite food is " + faveFood);
+
+
+var div = function(number1, number2) {
+  return number1 / number2;
+};
+
+var remainder = function(number1, number2) {
+	return number1 % number2
+}
+
+var threeTimes = function(number1, number2, number3){
+	return number1 * number2 * number3
+}
+*/
